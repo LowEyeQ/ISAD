1085f84d7bfccc8fafa6fb75acb6bfa6f055ad04
LowEyeQ <norrapat10@gmail.com>
2023-09-13 00:17:16 +0700

    esssssssssssss

--- website/__init__.py
+++ website/__init__.py
@@ -1,24 +1,25 @@
-from flask import Flask, render_template
-from flask_mysqldb import MySQL
-from config import DATABASE_CONFIG
+from flask import Flask
+# from flask_mysqldb import MySQL
+# from config import DATABASE_CONFIG
 
 app = Flask(__name__)
 
 # ใช้ข้อมูลการเชื่อมต่อ MySQL จาก config.py
-app.config['MYSQL_HOST'] = DATABASE_CONFIG['host']
-app.config['MYSQL_USER'] = DATABASE_CONFIG['user']
-app.config['MYSQL_PASSWORD'] = DATABASE_CONFIG['password']
-app.config['MYSQL_DB'] = DATABASE_CONFIG['database']
+# app.config['MYSQL_HOST'] = DATABASE_CONFIG['host']
+# app.config['MYSQL_USER'] = DATABASE_CONFIG['user']
+# app.config['MYSQL_PASSWORD'] = DATABASE_CONFIG['password']
+# app.config['MYSQL_DB'] = DATABASE_CONFIG['database']
 
-mysql = MySQL(app)
+# mysql = MySQL(app)
 
 @app.route('/')
 def indexs():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT  * FROM employee")  # เปลี่ยน your_table เป็นชื่อตารางของคุณ
-    data = cur.fetchall()
-    cur.close()
-    return render_template('index.html', data=data)
+    return "Hello world"
+    # cur = mysql.connection.cursor()
+    # cur.execute("SELECT  * FROM employee")  # เปลี่ยน your_table เป็นชื่อตารางของคุณ
+    # data = cur.fetchall()
+    # cur.close()
+    # return render_template('index.html', data=data)
 
 
 if __name__ == "__main__":
