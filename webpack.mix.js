d3fcfb3916ec1ced4531bb5c92786a23ba4cff55
LowEyeQ <norrapat10@gmail.com>
2023-09-13 19:47:51 +0700

    1

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css');
+mix.js('resources/js/app.js', 'public/js')  // ระบุโฟลเดอร์ปลายทางสำหรับไฟล์ JavaScript
+    .postCss('resources/css/app.css', 'public/css')  // ระบุโฟลเดอร์ปลายทางสำหรับไฟล์ CSS
+    
