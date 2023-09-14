8b2e284115fba0193a6596496fe9c074854da843
LowEyeQ <norrapat10@gmail.com>
2023-09-14 19:42:34 +0700

    test

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,3 @@
-const mix = require('laravel-mix');
+let mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js')  // ระบุโฟลเดอร์ปลายทางสำหรับไฟล์ JavaScript
+mix.js('src/app.js', 'dist').setPublicPath('dist');
